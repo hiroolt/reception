64e2bae0a5e9bac6f41cfbfca1806d415ff85c2c
hiroolt <sunasuna.8@gmail.com>
2017-12-31 15:47:19 +0900

    client.js updated

--- clients.js
+++ clients.js
@@ -1,9 +1,13 @@
 "use strict";
 var io = require('socket.io-client');
 var socket = io('http://192.168.2.105:3000').connect();
+var sound = require('node-aplay');
+
+// allow local file read
+var fs = require('fs');
 
 // receive call from the host
 socket.on('call', function(msg){
     console.log(msg);
-
+    new sound('assets/sounds/sample.wav').play();
 });
